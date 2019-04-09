9cee799985a7a9dcf7b44516cf8d977616303285
User16 <user16@user16de-imac.local>
2019-04-09 19:33:44 +0800

    Initial Commit

---
+++ IBOutlet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBOutlet
+//
+//  Created by User16 on 2019/4/9.
+//  Copyright © 2019 User16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
