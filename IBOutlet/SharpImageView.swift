b26ec2c8a065a5fe8766b3f91c789ec6f75386b2
Wilhelm Shen <33245687+WilhelmIllust@users.noreply.github.com>
2019-04-10 10:43:36 +0800

    first commit

---
+++ IBOutlet/SharpImageView.swift
@@ -0,0 +1,21 @@
+//
+//  SharpImageView.swift
+//  IBOutlet
+//
+//  Created by User16 on 2019/4/9.
+//  Copyright © 2019 User16. All rights reserved.
+//
+
+import UIKit
+
+class SharpImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ IBOutlet/startViewController.swift
@@ -0,0 +1,30 @@
+//
+//  startViewController.swift
+//  IBOutlet
+//
+//  Created by User16 on 2019/4/9.
+//  Copyright © 2019 User16. All rights reserved.
+//
+
+import UIKit
+
+class startViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
